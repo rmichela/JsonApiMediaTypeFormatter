dec70af2367a1958e8b06c0e221e5683a438c5b3
Ryan Michela <deltahat@gmail.com>
2015-04-19 18:21:51 -0700

    Unit tests

---
+++ JsonApi.Tests/ScratchTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace JsonApi.Tests
+{
+    [TestFixture]
+    public class ScratchTests
+    {
+        [Test]
+        public void ExpandosShouldDoSomething()
+        {
+            Assert.AreEqual("foo", "foo");
+        }
+    }
+}
